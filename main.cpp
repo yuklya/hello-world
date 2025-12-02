f8a630d998528fe1ae4bb55b99fd2493820f9d0d
yuklya <yuloseva@mail.ru>
2025-12-02 09:29:30 +0300

    start

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
-#include <iostream>
-int main()
-{
-  std::cout<< "Hello, top-it";
-  std::cout<<"\n";
+struct IDraw {
+  virtual p_t begin() const = 0; //Виртуальный метод, поэтому  равно 0
+  virtual p_t next(p_t p) const = 0;
+  virtual IDraw = default;
 }
